e15b3a51a13fabb7fe167bad1b9545b00e8ddd02
mizuka <trionfo1993@hotmail.com>
2017-12-15 10:24:08 +0800

    doc: 实例文件

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
